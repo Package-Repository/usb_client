7e43e65ee8c2e28c7820ffaea07ef69a5e1527ae
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-03-04 01:59:55 -0800

    Update Build Script

--- build_protocol.sh
+++ build_protocol.sh
@@ -9,13 +9,6 @@ fi
 mkdir build && cd build && cmake .. && make && sudo make install
 cd $start_dir
 
-cd hardware/sam
-if [ -d "build" ]; then
-    sudo rm -r build
-fi
-mkdir build && cd build && cmake .. && make
-cd $start_dir
-
 cd client_c
 if [ -d "build" ]; then
     sudo rm -r build
@@ -24,8 +17,3 @@ mkdir build && cd build && cmake .. && make && sudo make install
 cd $start_dir
 
 cp client_py/usb_protocol.py                $HOME/ROBOT_LIB
-cp client_py/read_usb.py                    $HOME/ROBOT_LIB
-cp hardware/sam/openocd_sam.cfg             $HOME/ROBOT_LIB
-cp hardware/sam/gdb_sam.txt                 $HOME/ROBOT_LIB
-cp hardware/sam/flash_sam.py                $HOME/ROBOT_LIB
-
